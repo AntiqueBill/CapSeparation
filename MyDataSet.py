9634a132c73e8113b26108a8ad2a33519bae886f
ZR1995 <1589402810@qq.com>
2019-10-15 09:42:53 +0800

    add .gitignore

--- MyDataSet.py
+++ MyDataSet.py
@@ -7,13 +7,10 @@ class MyDataSet(Dataset):
     def __init__(self, filepath):
         self.filepath = filepath
         self.allData = h5py.File(filepath,'r')
-<<<<<<< HEAD
+
         print(self.allData['x_test'].shape)
         print(self.allData['y_test'].shape)
-=======
-        print(self.allData['x_train'].shape)
-        print(self.allData['y_train'].shape)
->>>>>>> dfcf3b88dbdf22ea8ffde39d91b6e9ef58fe19e6
+
         #self.data = torch.Tensor(allData['x_train'])
         #print(self.data.shape)
         #self.labels = torch.Tensor(allData['x_train'])
@@ -21,23 +18,16 @@ class MyDataSet(Dataset):
 
 
     def __getitem__(self, item):
-<<<<<<< HEAD
-        x = self.allData['x_test'][item]
-        print('x.type',type(x))
-        print("x.shape",x.shape)
         y = self.allData['y_test'][item]
-=======
         x = np.transpose(self.allData['x_train'])[item]
         print('x.type',type(x))
         print("x.shape",x.shape)
         y = np.transpose(self.allData['y_train'])[item]
->>>>>>> dfcf3b88dbdf22ea8ffde39d91b6e9ef58fe19e6
         print("y.shape",y.shape)
         print('y.type', type(y))
         return x, y
 
     def __len__(self):
-<<<<<<< HEAD
         return len(np.transpose(self.allData['x_test']))
 
 if __name__ == '__main__':
@@ -51,16 +41,3 @@ if __name__ == '__main__':
         print(type(x_test))
         print(x_test.shape)
         print(y_test.shape)
-=======
-        return len(np.transpose(self.allData['x_train']))
-
-if __name__ == '__main__':
-    dataset = MyDataSet('./train_receiver.mat')
-    print(len(dataset))
-    train_loader = torch.utils.data.DataLoader(dataset,batch_size=128, shuffle=True)
-    for batch_idx, (data, target) in enumerate(train_loader):
-        print(batch_idx)
-        print(type(data))
-        print(data.shape)
-        print(target)
->>>>>>> dfcf3b88dbdf22ea8ffde39d91b6e9ef58fe19e6
